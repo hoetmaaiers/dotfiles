bc8a844b32da6166ef34ed53ad542e4bb56986e8
Robin Houdmeyers <robin.houdmeyers@vito.be>
2025-11-27 10:25:11 +0100

    Add sane defaults to .macos

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+--
+--
+-- remove LazyVim's leap keymaps
+vim.keymap.del({ "n", "x", "o" }, "s")
+vim.keymap.del({ "n", "x", "o" }, "S")
