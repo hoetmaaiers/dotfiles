8aa3f89e413f0d9ee9d32f4296f983277b44ab84
Robin Houdmeyers <robin.houdmeyers@vito.be>
2025-12-01 09:34:51 +0100

    Update fish config for improved prompt host styling
    Update fish prompt with better conditionals and guard handling
    
    Update fish prompt
    
    Refactor fish prompt to remove host alias from display

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,14 +1,23 @@
 function fish_prompt --description 'Write out the prompt'
-
     set host (hostname)
-    if string match -r "s12-hpc.*" $host
-        set prefix "🔥 CLUSTER-NODE"
-        echo -n "$prefix $host "
-    else
-        set prefix "💻"
-        echo -n "$prefix $host "
+
+    switch $host
+        case 's12-hpc*'
+            # show full hostname for cluster nodes
+            set host_alias $host
+            set prefix (set_color bryellow)'⚡'(set_color normal)
+        case 'develop.marvin.vito.local'
+            set host_alias DEV
+            set prefix (set_color bryellow)'⚡'(set_color normal)
+        case 'login.marvin.vito.local'
+            set host_alias LOGIN
+            set prefix (set_color bryellow)'⚡'(set_color normal)
+        case '*'
+            set host_alias $host
+            set prefix (set_color brgreen)'🏡'(set_color normal)
     end
 
+    echo -n "$prefix "
     set -l last_status $status
     set -l normal (set_color normal)
 
@@ -17,11 +26,9 @@ function fish_prompt --description 'Write out the prompt'
     set -l vcs_color (set_color brpurple)
     set -l prompt_status ""
 
-    # Since we display the prompt on a new line allow the directory names to be longer.
     set -q fish_prompt_pwd_dir_length
     or set -lx fish_prompt_pwd_dir_length 0
 
-    # Color the prompt differently when we're root
     set -l suffix '❯'
     if functions -q fish_is_root_user; and fish_is_root_user
         if set -q fish_color_cwd_root
@@ -30,10 +37,9 @@ function fish_prompt --description 'Write out the prompt'
         set suffix '#'
     end
 
-    # Color the prompt in red on error
     if test $last_status -ne 0
         set status_color (set_color $fish_color_error)
-        set prompt_status $status_color "[" $last_status "]" $normal
+        set prompt_status $status_color"["$last_status"]"$normal
     end
 
     echo -s (prompt_login) ' ' $cwd_color (prompt_pwd) $vcs_color (fish_vcs_prompt) $normal ' ' $prompt_status
